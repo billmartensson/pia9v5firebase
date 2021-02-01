c067b2d39a900b5432df70d686aa2abfa6fd27d6
Bill Mårtensson <bill@magictechnology.se>
2021-02-01 10:06:51 +0100

    Start

---
+++ app/src/main/java/se/magictechnology/pia9v5firebase/MainActivity.kt
@@ -0,0 +1,76 @@
+package se.magictechnology.pia9v5firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "pia9debug"
+
+    private lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        database = Firebase.database.reference
+
+
+
+        /*
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<String>()
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+        */
+
+
+        val banana = Fruit("Banan", "Gul")
+
+        database.child("fruitsalad").setValue(banana)
+
+
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                val post = dataSnapshot.getValue<Fruit>()
+                // ...
+                Log.d(TAG, post!!.fruitname!!)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+                // ...
+            }
+        }
+        database.child("fruitsalad").addValueEventListener(postListener)
+    }
+}
+
+data class Fruit(var fruitname : String? = "", var fruitcolor : String? = "")
